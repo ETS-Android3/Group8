af696d6395fc492d99a750caa7e6cabbb0220fe2
Tyler Sanbar <tylersanbar@gmail.com>
2021-12-05 15:31:52 -0600

    Fleshed out tests and attempts

--- SecurityApp/app/src/main/java/com/example/SecurityApp/Attempt.java
+++ SecurityApp/app/src/main/java/com/example/SecurityApp/Attempt.java
@@ -3,9 +3,31 @@ package com.example.SecurityApp;
 import java.util.ArrayList;
 
 public class Attempt {
-    private User user;
-    private int milliseconds;
+    private int attemptTime;
     private String lockType;
     private boolean unlockSuccess;
-    private ArrayList<String> unlockPattern;
+    private String unlockPattern;
+
+    public Attempt(int attemptTime, String lockType, boolean unlockSuccess, String unlockPattern) {
+        this.attemptTime = attemptTime;
+        this.lockType = lockType;
+        this.unlockSuccess = unlockSuccess;
+        this.unlockPattern = unlockPattern;
+    }
+
+    public int getAttemptTime() {
+        return attemptTime;
+    }
+
+    public String getLockType() {
+        return lockType;
+    }
+
+    public boolean isUnlockSuccess() {
+        return unlockSuccess;
+    }
+
+    public String getUnlockPattern() {
+        return unlockPattern;
+    }
 }
--- SecurityApp/app/src/main/java/com/example/SecurityApp/Test.java
+++ SecurityApp/app/src/main/java/com/example/SecurityApp/Test.java
@@ -4,4 +4,13 @@ import java.util.ArrayList;
 
 public class Test {
     private ArrayList<Attempt> attempts;
+    private int uid;
+    public Test(ArrayList<Attempt> attempts, int uid) {
+        this.attempts = attempts;
+        this.uid = uid;
+    }
+
+    public ArrayList<Attempt> getAttempts() {
+        return attempts;
+    }
 }
