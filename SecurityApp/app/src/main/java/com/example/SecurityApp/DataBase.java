da42e21fbfbfa85827192ef8aba675e36c07896a
Parker Brandt <parker.brandt101@gmail.com>
2021-12-04 21:35:11 -0600

    Added async functionality for Database access

--- SecurityApp/app/src/main/java/com/example/SecurityApp/DataBase.java
+++ SecurityApp/app/src/main/java/com/example/SecurityApp/DataBase.java
@@ -8,14 +8,14 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 
-public class DataBase{
+public class DataBase {
+    // Properties
+    ArrayList<User> users;
+
     // Database credentials
     final static String HOSTNAME = "sanb4019-sql-server.database.windows.net";
-
     final static String DBNAME = "security_project";
-
     final static String USERNAME = "sanb4019";
-
     final static String PASSWORD = "5VEXue6kJzUN7jZ";
 
     // Database connection string
@@ -28,7 +28,12 @@ public class DataBase{
     PreparedStatement preparedStatement = null;
     ResultSet resultSet = null;    // For the result set, if applicable
 
-    ArrayList<User> users;
+    // Query Templates
+    private final static String GET_USERS_QUERY = "SELECT name, id FROM USERS;";
+    private final static String DELETE_USERS_QUERY = "DELETE FROM USERS;";
+    private final static String INSERT_USER_QUERY = "INSERT INTO USERS VALUES (?, ?);";
+
+
     public DataBase() {
         users = getRemoteUsers();
     }
@@ -36,6 +41,7 @@ public class DataBase{
     public ArrayList<User> getUsers() {
         return users;
     }
+
     public ArrayList<String> getUserNames(){
         ArrayList<String> user_names = new ArrayList<>();
         for(User user:users){
@@ -43,6 +49,7 @@ public class DataBase{
         }
         return user_names;
     }
+
     public boolean addUser(String name){
         boolean nameExists = false;
         for(User u:users){if(u.getName().equals(name)) nameExists = true;}
@@ -59,58 +66,57 @@ public class DataBase{
         setRemoteUsers();
     }
 
-    private void setRemoteAll(){
+    private void setRemoteAll(){ }
 
-    }
-    private ArrayList<User> getRemoteUsers(){
-
+    private ArrayList<User> getRemoteUsers() {
         ArrayList<User> users = new ArrayList<>();
-        try
-        {
-            // Establish the connection.
-            connection = DriverManager.getConnection(URL);
-            String sql = "SELECT name,id FROM USERS;";
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery(sql);
 
-            while(resultSet.next())
-            {
-                users.add(new User(resultSet.getString(1),resultSet.getInt(2)));
-                //System.out.println("GOT USER");
+        // Perform the Database operation async, to not slow down the app
+        Thread thread = new Thread( () -> {
+            try {
+                // Establish the connection.
+                connection = DriverManager.getConnection(URL);
+                statement = connection.createStatement();
+                resultSet = statement.executeQuery(GET_USERS_QUERY);
+
+                while(resultSet.next())
+                {
+                    users.add(new User(resultSet.getString(1),resultSet.getInt(2)));
+                }
+                System.out.println("Got Users");
+                System.out.println(users);
+                connection.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
             }
-            System.out.println("Got Users");
-            System.out.println(users);
-            connection.close();
-        }
-        catch (Exception ex)
-        {
-            ex.printStackTrace();
-        }
+        });
 
         return users;
     }
+
     private void setRemoteUsers(){
-        try
-        {
-            // Establish the connection.
-            connection = DriverManager.getConnection(URL);
-            String sql_delete = "DELETE FROM USERS;";
-            String sql_insert = "INSERT INTO USERS VALUES (?, ?);";
-            statement = connection.createStatement();
-            statement.executeUpdate(sql_delete);
-            //Insert each user
-            for (User user:users) {
-                preparedStatement = connection.prepareStatement(sql_insert);
-                preparedStatement.setInt(1, user.getId());
-                preparedStatement.setString(2, user.getName());
-                preparedStatement.executeUpdate();
+
+        Thread thread = new Thread( () -> {
+            try {
+                // Establish the connection.
+                connection = DriverManager.getConnection(URL);
+                statement = connection.createStatement();
+
+                // Delete all users for fresh start
+                statement.executeUpdate(DELETE_USERS_QUERY);
+
+                //Insert each user
+                for (User user:users) {
+                    preparedStatement = connection.prepareStatement(INSERT_USER_QUERY);
+                    preparedStatement.setInt(1, user.getId());
+                    preparedStatement.setString(2, user.getName());
+                    preparedStatement.executeUpdate();
+                }
+                System.out.println("Inserted Users.");
+                connection.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
             }
-            System.out.println("Inserted Users.");
-            connection.close();
-        }
-        catch (Exception ex)
-        {
-            ex.printStackTrace();
-        }
+        });
     }
 }
